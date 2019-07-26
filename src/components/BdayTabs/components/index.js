ffaa11822c370ac0231e86db108733578baf09b0
DenisBelkin <merockersiv@gmail.com>
2019-07-26 17:28:55 +0300

    main functionality v1.0.0

--- src/components/BdayTabs/components/index.js
+++ src/components/BdayTabs/components/index.js
@@ -1,10 +1,19 @@
+/**
+ * TODO:
+ * 1) check and fix cases with leap year
+ * 2) check users cards for correct data printing (CHECK TASK CONDITIONS!!!)
+ * 3) insert links to ./common
+ * 4) check for adaptive mark-up
+ * 5) refactoring
+ * 6) code cleaning
+ * */
+
+
 import React, {PureComponent} from 'react';
 import {Card, Avatar, Spin} from 'antd';
 import moment from 'moment';
 import isEmpty from 'lodash/isEmpty';
 import axios from 'axios';
-
-const {Meta} = Card;
 
 import {
     StyledTabsContainer as Container,
@@ -14,48 +23,69 @@ import {
     StyledCard
 } from './styled';
 
+const {Meta} = Card;
+
+const DATES_TYPES = {
+    PREV_DATES: 'prevDates',
+    TODAY_DATES: 'todayDates',
+    NEXT_DATES: 'nextDates'
+};
+
 export default class BdayTabs extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             data: [],
-            isLoading:true,
+            isLoading: true,
         }
 
     }
 
-    componentDidMount(){
-        const date = this.getDate()
-        console.error(date);
-
-        //place for func which will use moment.js to get 2prev weeks and 2next weeks; that func will return obj:{prev:'07.15',today:07.29',next:'08.12'}
-
-        axios.get(`http://test.anromsocial.com/api/birthdays?dateFrom=${date.prev}&dateTo=${date.next}`).then( res => {
-            console.log('response: ',res)
-            this.setState({
-                isLoading:false,
-                data: res.data.users
-            });
-        })
+    async componentDidMount() {
+        const data = await this.dateRequests();
+        this.setState({isLoading: false, data})
     }
 
-    getDate(){
+    dateRequests = async () => {
+        const date = this.getDate();
+
+        const prevUsers = axios.get(`http://test.anromsocial.com/api/birthdays?dateFrom=${date.prev}&dateTo=${date.currentDate.onePrev}`);
+        const todayUsers = axios.get(`http://test.anromsocial.com/api/birthdays?dateFrom=${date.currentDate.today}&dateTo=${date.currentDate.today}`);
+        const nextUsers = axios.get(`http://test.anromsocial.com/api/birthdays?dateFrom=${date.currentDate.oneNext}&dateTo=${date.next}`);
+
         return {
-            prev: moment().subtract(2,'weeks').format('MM.DD'),
-            today: moment().format('MM.DD'),
-            next: moment().add(2,'weeks').format('MM.DD')
+            [DATES_TYPES.PREV_DATES]: await prevUsers,
+            [DATES_TYPES.TODAY_DATES]: await todayUsers,
+            [DATES_TYPES.NEXT_DATES]: await nextUsers,
+        };
+    };
+
+    getDate() {
+        const format = 'MM.DD';
+        return {
+            prev: moment().subtract(2, 'weeks').format(format),
+            currentDate: {
+                onePrev:moment().subtract(1,'days').format(format),
+                today:moment().format(format),
+                oneNext: moment().add(1, 'days').format(format)
+            },
+            next: moment().add(2, 'weeks').format(format)
         }
     }
 
 
-    renderCards(){
+    renderCards(type) {
         const {data} = this.state;
-        return isEmpty(data) ? (<div style={{width:'100%', textAlign: 'center'}}><i>Sorry, no data to show</i></div>): data.map((item,key)=>{
+        const currentData = data[type].data.users;
+        return isEmpty(currentData) ?
+            (<div style={{width: '100%', textAlign: 'center'}}><i>Sorry, no data to show</i>
+        </div>)
+            : currentData.map((item, key) => {
             return (
                 <StyledCard key={key} size={'small'} bordered={false}>
                     <Meta
                         avatar={
-                            <Avatar size={64} src={`http://test.anromsocial.com/${item.avatarUrl}`} />
+                            <Avatar size={64} src={`http://test.anromsocial.com/${item.avatarUrl}`}/>
                         }
                         title={item.name}
                         description={<div>
@@ -70,53 +100,22 @@ export default class BdayTabs extends PureComponent {
     }
 
     render() {
-        console.log(this.state)
-
-        return this.state.isLoading ? <div style={{width:'100%', textAlign:'center'}}><Spin size={'large'} /></div>:(
+        return this.state.isLoading ? <div style={{width: '100%', textAlign: 'center'}}><Spin size={'large'}/></div> : (
             <Container>
                 <Tabs defaultActiveKey={'1'}>
                     <TabPane key={'0'} tab={'RECENT DATES'}>
                         <PaneContent>
-                            123 perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi
-                            architecto
-                            beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
-                            aspernatur
-                            aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi
-                            nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur,
-                            adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam
-                            aliquam quaerat volu
+                            {this.renderCards(DATES_TYPES.PREV_DATES)}
                         </PaneContent>
                     </TabPane>
                     <TabPane key={'1'} tab={'TODAY'}>
                         <PaneContent>
-                            {this.renderCards()}
-
+                            {this.renderCards(DATES_TYPES.TODAY_DATES)}
                         </PaneContent>
                     </TabPane>
                     <TabPane key={'2'} tab={'UPCOMING DATES'}>
                         <PaneContent>
-
-                            545Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi
-                            architecto
-                            beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
-                            aspernatur
-                            aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi
-                            nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur,
-                            adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam
-                            aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-                            corporis
-                            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure
-                            reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum,
-                            qui
-                            dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio
-                            dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos
-                            dolores
-                            et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in
-                            culpa,
-                            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
-                            facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis es
+                            {this.renderCards(DATES_TYPES.NEXT_DATES)}
                         </PaneContent>
                     </TabPane>
                 </Tabs>
